2a3ed4c7fd84ae60cb216a9a985b53858a8f163e
Gherasimuru-san <19626576+gherazaurul@users.noreply.github.com>
2024-03-06 23:24:35 +0200

    added services w/ dependency injection

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	//"fmt"
 	"platform/config"
 	"platform/logging"
+	"platform/services"
 )
 
 func writeMessage(logger logging.Logger, cfg config.Configuration) {
@@ -23,15 +24,10 @@ func writeMessage(logger logging.Logger, cfg config.Configuration) {
 }
 
 func main() {
-	var cfg config.Configuration
-	var err error
-
-	cfg, err = config.Load("config.json")
-
-	if err != nil {
-		panic(err)
-	}
-
-	var logger logging.Logger = logging.NewDefaultLogger(cfg)
-	writeMessage(logger, cfg)
+	services.RegisterDefaultServices()
+	// var cfg config.Configuration
+	// services.GetService(&cfg)
+	// var logger logging.Logger
+	// services.GetService(&logger)
+	services.Call(writeMessage)
 }
